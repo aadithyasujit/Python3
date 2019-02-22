f74cdc79224d4b71e99380b4d611881d6f9db107
aadithyasujit <aadithyasujit@gmail.com>
2019-02-22 17:37:35 +0530

    Included pair listing trial !!

--- Array.py
+++ Array.py
@@ -1,27 +1,24 @@
-
 Myarr = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 sumto = int(15)
 
 
-def Sumcheck (Myarr, sumto):
-
+def Sumcheck(Myarr, sumto):
     count = int(0)
     First = []
     Second = []
     for i in range(len(Myarr)):
 
-        #print(Myarr[i])
+        # print(Myarr[i])
 
         for j in range(len(Myarr)):
 
             if (i == j):
 
-                j=j+1
+                j = j + 1
 
             else:
 
                 if (Myarr[i] + Myarr[j] == sumto):
-
                     First.append(Myarr[i])
                     Second.append(Myarr[j])
                     count = count + 1
@@ -29,5 +26,11 @@ def Sumcheck (Myarr, sumto):
         continue
 
     print(count, "pairs sum up to", sumto)
-    print(First,",",Second)
+
+    for i in range(len(First)):
+        print("Pair",i+1,":[", First[i], ",", Second[i],"]")
+
+        continue
+
+
 Sumcheck(Myarr, sumto)
