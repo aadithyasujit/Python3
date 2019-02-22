4e9f3ad8e65b08857f1f251feb489c672cd131c6
aadithyasujit <aadithyasujit@gmail.com>
2019-02-22 16:41:53 +0530

    First step

---
+++ Array.py
@@ -0,0 +1,33 @@
+
+Myarr = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+sumto = int(15)
+
+
+def Sumcheck (Myarr, sumto):
+
+    count = int(0)
+    First = []
+    Second = []
+    for i in range(len(Myarr)):
+
+        #print(Myarr[i])
+
+        for j in range(len(Myarr)):
+
+            if (i == j):
+
+                j=j+1
+
+            else:
+
+                if (Myarr[i] + Myarr[j] == sumto):
+
+                    First.append(Myarr[i])
+                    Second.append(Myarr[j])
+                    count = count + 1
+
+        continue
+
+    print(count, "pairs sum up to", sumto)
+    print(First,",",Second)
+Sumcheck(Myarr, sumto)
